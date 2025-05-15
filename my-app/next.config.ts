760663ccb8082bec33ff25c9634b8205c7297f30
Datav1nci <jestais4@gmail.com>
2025-05-15 10:50:31 -0400

    rebuild

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
+  basePath: '/Project1', // Matches your GitHub Pages URL (https://datav1nci.github.io/Project1/)
 };
 
 export default nextConfig;
