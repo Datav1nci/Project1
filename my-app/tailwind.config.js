1b6a71ab3330d1578d5e23cdadd3dcef5eb8b836
Datav1nci <jestais4@gmail.com>
2025-05-15 15:13:12 -0400

    undo changes du to error

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', 
+module.exports = { 
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
