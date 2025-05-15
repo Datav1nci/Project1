4bba23b23b1cb9fe8b92e3bb8ee4f79fe9476793
Datav1nci <jestais4@gmail.com>
2025-05-15 10:52:45 -0400

    playing around with config file

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: '/Project1', // Matches your GitHub Pages URL (https://datav1nci.github.io/Project1/)
+  basePath: '/Project1/my-app', // Matches your GitHub Pages URL (https://datav1nci.github.io/Project1/)
 };
 
 export default nextConfig;
